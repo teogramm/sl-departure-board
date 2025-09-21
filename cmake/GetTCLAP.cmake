2daf925cae569c5aa745e0362d499922d73c18c2
Theodoros Grammenos <teogramm@outlook.com>
2025-09-21 10:56:01 +0200

    [deps] Correct TCLAP CMake variable types

--- cmake/GetTCLAP.cmake
+++ cmake/GetTCLAP.cmake
@@ -5,7 +5,7 @@ FetchContent_Declare(
         GIT_TAG 1.4
         EXCLUDE_FROM_ALL
 )
-set(BUILD_EXAMPLES OFF)
-set(BUILD_UNITTESTS OFF)
-set(BUILD_DOC OFF)
+set(BUILD_EXAMPLES OFF CACHE BOOL "")
+set(BUILD_UNITTESTS OFF CACHE BOOL "")
+set(BUILD_DOC OFF CACHE BOOL "")
 FetchContent_MakeAvailable(tclap)
